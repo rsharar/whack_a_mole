31f387261344c7164e8c3545f6a10fa6c4fadc48
Rudy Sharar <rudy@activelylearn.com>
2018-11-27 19:29:18 -0800

    init files

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS project_two_db;
+CREATE DATABASE project_two_db;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
